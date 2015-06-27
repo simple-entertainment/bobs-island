f8d398d394280c892db12f3bb060978a2e497fbb
Gary Buyn <simplegsb@gmail.com>
2015-06-27 16:00:54 +0800

    Used submodules/source trees for dependencies

--- dep-linux.sh
+++ dep-linux.sh
@@ -1,1 +1,1 @@
-sudo apt-get install g++ git git-gui libgl1-mesa-dev libglu1-mesa-dev libglew-dev freeglut3-dev libfreeimage-dev
+sudo apt-get install g++ git git-gui libgl1-mesa-dev libglu1-mesa-dev
